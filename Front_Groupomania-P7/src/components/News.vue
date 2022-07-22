82ada2b9960063278c9caaca765a09e06f66db2e
Alpha1788 <jblamotte1104@gmail.com>
2022-07-22 17:45:37 +0200

    rajout du compte admin dans la BDD

--- Front_Groupomania-P7/src/components/News.vue
+++ Front_Groupomania-P7/src/components/News.vue
@@ -2,9 +2,9 @@
   <div class="container-sm ">
     <nav class=" navbar navbar-expand-lg navbar-light bg-light mt-3">
       <div class="container-fluid bg-light">
-        <h1 class="navbar-brand">Groupomania </h1>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <a href="/Home.vue"><h1 class="navbar-brand">Groupomania</h1>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -70,8 +70,8 @@
 
 
 <script>
-function logout() {//_________________________ Logout user
-
+// Bouton Deconnexion
+function logout() {
   localStorage.removeItem("token");
   localStorage.removeItem("userId");
   localStorage.removeItem("email");
